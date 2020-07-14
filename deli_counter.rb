ee021e87beaedd71abf2a8d8261eda0f4edf94d4
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-14 18:10:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+katz_deli = []
+
+def line(i)
+  new_array = []
+  if i.length == 0 
+    puts "The line is currently empty."
+  
+  elsif i.length >= 1
+  i.each_with_index do |name, index|
+  i[1] = #{index}
+  new_array << "The line is currently #{index}. #{name}"
+  end
+end 
+
+def take_a_number(array, name)
+  
+ array.push(name)
+  counter = 1
+  
+  array.each_with_index do|name|
+   counter += 1
+  end
+ puts "Welcome, #{name}. You are number #{array.length} in line."
+  end
+end
+
+
+  
